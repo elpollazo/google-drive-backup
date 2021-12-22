9eb9150a47199bbc04e0faf8cf3197e750a7c34e
Sebastián <57805712+elpollazo@users.noreply.github.com>
2021-12-21 22:12:48 -0300

    Update backup.py

--- backup/backup.py
+++ backup/backup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+"""
+This python script uploads all files in a specific folder locally to a specific folder in Google Drive. 
+You can run it automaticaly as a crontab job or when the system boots with systemd. 
+The script was tested on Linux, I don't discard the functionallity on Windows.
+"""
+
 import os
 import sys
 
